4d4dc72debd100605cf150ef1cdab9a01d468d0c
Kevin Chen <ksrchen@Kevins-MacBook-Pro.local>
2012-12-30 10:14:01 -0800

    Initial Commit

---
+++ homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  homepwner
+//
+//  Created by Kevin Chen on 12/30/12.
+//  Copyright (c) 2012 Kevin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  homepwner
+//
+//  Created by Kevin Chen on 12/30/12.
+//  Copyright (c) 2012 Kevin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HomepwnerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HomepwnerAppDelegate class]));
+    }
+}
